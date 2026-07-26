fdb10931c186c47a8bcf8a1a8bea4c1ab577da2e
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-26 11:06:06 -0500

    Reasignar valores de get y set en clases testVehiculo-Vehiculo

--- src/dom/krakedev/TestVehiculo.java
+++ src/dom/krakedev/TestVehiculo.java
@@ -7,35 +7,37 @@ public class TestVehiculo {
 		v = new Vehiculo();
 
 		System.out.println("===========Vehiculo1: Creado===========");
-		System.out.println("Año: " + v.anio);
-		System.out.println("Vehiculo: " + v.marca);
-		System.out.println("Modelo: " + v.modelo);
-		System.out.println("Valor: " + v.valor);
+		System.out.println("Año: " + v.getAnio());
+		System.out.println("Vehiculo: " + v.getMarca());
+		System.out.println("Modelo: " + v.getModelo());
+		System.out.println("Valor: " + v.getValor());
 		System.out.println("===========Vehiculo1: Valores Asicnados===========");
-		v.anio = "2012";
-		v.marca = "Chevrolet";
-		v.modelo = "Nuevo";
-		v.valor = "$24.000";
-		System.out.println("Año: " + v.anio);
-		System.out.println("Vehiculo: " + v.marca);
-		System.out.println("Modelo: " + v.modelo);
-		System.out.println("Valor: " + v.valor);
+		v.setAnio("2002"); 
+		v.setMarca("Chevrolet"); 
+		v.setModelo("Nuevo");
+		v.setValor("$24.000");
+		System.out.println("Año: " + v.getAnio());
+		System.out.println("Vehiculo: " + v.getMarca());
+		System.out.println("Modelo: " + v.getModelo());
+		System.out.println("Valor: " + v.getValor());
 		System.out.println("===========Vehiculo2: Creado===========");
 
 		Vehiculo v2 = new Vehiculo();
-		System.out.println("Año: " + v2.anio);
-		System.out.println("Vehiculo: " + v2.marca);
-		System.out.println("Modelo: " + v2.modelo);
-		System.out.println("Valor: " + v2.valor);
-		v2.anio = "2013";
-		v2.marca = "Mazda";
-		v2.modelo = "Viejo";
-		v2.valor = "$14.000";
+		System.out.println("Año: " + v2.getAnio());
+		System.out.println("Vehiculo: " + v2.getMarca());
+		System.out.println("Modelo: " + v2.getModelo());
+		System.out.println("Valor: " + v2.getValor());
+		
+		v2.setAnio("2013"); 
+		v2.setMarca("Mazda"); 
+		v2.setModelo("Viejo");
+		v2.setValor("$14.000");
+		
 		System.out.println("===========Vehiculo2: Valores Asicnados===========");
-		System.out.println("Año: " + v2.anio);
-		System.out.println("Vehiculo: " + v2.marca);
-		System.out.println("Modelo: " + v2.modelo);
-		System.out.println("Valor: " + v2.valor);
+		System.out.println("Año: " + v2.getAnio());
+		System.out.println("Vehiculo: " + v2.getMarca());
+		System.out.println("Modelo: " + v2.getModelo());
+		System.out.println("Valor: " + v2.getValor());
 	}
 
 }
