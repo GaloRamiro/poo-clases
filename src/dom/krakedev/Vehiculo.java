badf52497e9869183e6140399058dc0b9b7072f9
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-26 10:59:12 -0500

    Clase vehiculo crear get set

--- src/dom/krakedev/Vehiculo.java
+++ src/dom/krakedev/Vehiculo.java
@@ -1,10 +1,34 @@
 package dom.krakedev;
 
 public class Vehiculo {
-	 public String marca;
-	 public String modelo;
-	 public String anio;
-	 public String valor;
+	 private String marca;
+	 private String modelo;
+	 private String anio;
+	 private String valor;
+	 public String getMarca() {
+		 return marca;
+	 }
+	 public void setMarca(String marca) {
+		 this.marca = marca;
+	 }
+	 public String getModelo() {
+		 return modelo;
+	 }
+	 public void setModelo(String modelo) {
+		 this.modelo = modelo;
+	 }
+	 public String getAnio() {
+		 return anio;
+	 }
+	 public void setAnio(String anio) {
+		 this.anio = anio;
+	 }
+	 public String getValor() {
+		 return valor;
+	 }
+	 public void setValor(String valor) {
+		 this.valor = valor;
+	 }
 	 
 
 	}
